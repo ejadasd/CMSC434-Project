98241db0ddcdf6e1b93438e7c66dc326f18b4110
Hunter Webb <hwebb1@terpmail.umd.edu>
2023-10-30 17:50:03 -0400

    Fixed home screen page sizing, separated html css and js into own files for page. modified footer to use icons instead of words

---
+++ HomeScreen.js
@@ -0,0 +1,29 @@
+function openProfilePage() {
+    window.location.href = 'profilepage.html';
+}
+
+document.getElementById('userName').textContent = localStorage.getItem('name') || '';
+
+document.getElementById('profile-icon').addEventListener('click', openProfilePage);
+
+// Rest of your JavaScript code
+function openTab(event, tabName) {
+    // Get all elements with class "box" and hide them
+    var tabs = document.querySelectorAll(".box");
+    tabs.forEach(function (tab) {
+        tab.style.display = "none";
+    });
+
+    // Get all elements with class "tab-button" and remove the "active" class
+    var tabButtons = document.querySelectorAll(".tab-button");
+    tabButtons.forEach(function (button) {
+        button.classList.remove("active");
+    });
+
+    // Show the selected tab and set the button as "active"
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.classList.add("active");
+}
+
+// Initially, show the Home tab and set the Home button as "active"
+openTab(null, "homeScreen");
