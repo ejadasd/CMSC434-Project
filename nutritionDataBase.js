81eeb2317219fd71783c7c8e842d0e71ea1affed
Huu Nguyen <nguyensteven2608@gmail.com>
2023-11-02 18:33:52 -0400

    Added more functionality to app

--- nutritionDataBase.js
+++ nutritionDataBase.js
@@ -1,6 +1,6 @@
 const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector('.search-result');
-const container = document.querySelector('c.ontainer');
+const container = document.querySelector('.container');
 let searchQuery = '';
 const APP_ID = 'f7fc2024';
 const APP_key = '0eed8476b12866be3ebb676637ee23b2';
@@ -18,6 +18,7 @@ async function fetchAPI() {
     const data = await response.json();
     generateHTML(data.hits);
 }
+
 function generateHTML(results){
 
     let generatedHTML = '';
@@ -29,6 +30,7 @@ function generateHTML(results){
             <div class="flex-container">
                 <h1 class="title">${result.recipe.label}</h1>
                 <a class="view-button" href="${result.recipe.url}" target="_blank">View Recipe</a>
+                <button onclick="save('${result.recipe.url}','${result.recipe.label}');"><ion-icon name="bookmark-outline"></ion-icon></button>
             </div>
             <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
             <p class="item-data">Diet label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : 'No Data Found'}</p>
@@ -37,4 +39,38 @@ function generateHTML(results){
         `
     })
     searchResultDiv.innerHTML = generatedHTML;
+}
+
+function save(link, label) {
+    var new_data = {name: label, url: link};
+
+    if (localStorage.getItem('data') == null) {
+        localStorage.setItem('data', '[]');
+    }
+
+    var old_data = JSON.parse(localStorage.getItem('data'));
+    old_data.push(new_data);
+
+    localStorage.setItem('data', JSON.stringify(old_data));
+}
+
+function view() {
+    var list = [];
+    if (localStorage.getItem('data') != null) {
+        list = JSON.parse(localStorage.getItem('data'));
+    }
+    let genHTML = '';
+    for (let value of list) {
+        genHTML += 
+        `
+        <div class="item">
+            <div class="flex-container">
+                <h1 class="title">${value.name}</h1>
+                <a class="view-button" href="${value.url}" target="_blank">View Recipe</a>
+            </div>
+        </div>
+        `;
+    }
+
+    searchResultDiv.innerHTML = genHTML;
 }
