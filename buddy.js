e0fb182fd9b521e83b18fc76d70d3927f0c0e43c
Huu Nguyen <nguyensteven2608@gmail.com>
2023-11-09 20:38:56 -0500

    fix buddy js

--- buddy.js
+++ buddy.js
@@ -1,7 +1,38 @@
-let inputs = document.getElementById("inp");
-let text = document.querySelector(".text");
+//let inputs = document.getElementById("inp");
+let text = document.querySelector(".friend-list");
+const itemsArray = localStorage.getItem('friend_list') ? JSON.parse(localStorage.getItem('friend_list')) : [];
 
 
+document.querySelector("#enter").addEventListener("click", () => {
+    const item = document.querySelector("#inp")
+    createItem(item)
+})
+
+document.querySelector("#inp").addEventListener("keypress", (e) => {
+    if(e.key === "Enter"){
+      const item = document.querySelector("#inp")
+      createItem(item)
+    }
+})
+
+
+function displayItems(){
+    //let items = ""
+    for(let i = 0; i < itemsArray.length; i++){
+        let items = document.createElement("ul");
+        items.innerHTML = `${itemsArray[i]} <i class="fa-solid fa-trash deleteBtn"></i>`
+        text.appendChild(items);
+    }
+    activateDeleteListeners()
+}
+
+
+function activateDeleteListeners(){
+    let deleteBtn = document.querySelectorAll(".deleteBtn")
+    deleteBtn.forEach((dB, i) => {
+      dB.addEventListener("click", () => { deleteItem(i) })
+    })
+}
 
 function Add(){
     if(inputs.value == ""){
@@ -17,3 +48,19 @@ function Add(){
         }
     }
 }
+
+function createItem(item){
+    itemsArray.push(item.value)
+    localStorage.setItem('friend_list', JSON.stringify(itemsArray))
+    location.reload()
+}
+  
+function deleteItem(i){
+    itemsArray.splice(i,1)
+    localStorage.setItem('friend_list', JSON.stringify(itemsArray))
+    location.reload()
+}
+
+window.onload = function() {
+    displayItems()
+};
